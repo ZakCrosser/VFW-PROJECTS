221b0b7e7fa041f4f36842441f68a3259d4ec4a1
ZakCrosser <zakcrosser@gmail.com>
2012-08-05 12:00:06 -0600

    creating select menu in js
    deleting select menu in HTML and making one with JS,
    Changes to css colors

--- Project 2/js/main.js
+++ Project 2/js/main.js
@@ -9,13 +9,31 @@ window.addEventListener("DOMContentLoaded", function(){
     return element;
   }
   
-  var displayLink = $('displayLink');
+  function makeDropMenu(){
+    var formTag = document.getElementsByTagName("form"),
+        selectLi = $('category'),
+        makeSelect = document.createElement('select');
+        makeSelect.setAttribute("id", "groups");
+    for(var i=0, j=catagory.length; i<j; i++){
+      var makeOption = document.createElement('option');
+      var optText = catagory[i];
+      makeOption.setAttribute("value", optText);
+      makeOption.innerHTML = optText;
+      makeSelect.appendChild(makeOption);
+    }
+    selectLi.appendChild(makeSelect);
+  }
+  
+  var catagory = ["--Choose a Catagory--", "Food", "Credit Card", "Entertainment", "ATM Withdraw"];
+  makeDropMenu();
+  
+  /* var displayLink = $('displayLink');
   displayLink.addeventListener("click", showData);
   var clearLink = $("clear");
   clearLink.addEventListener("click", clearData);
   var addTransaction = $(addTransaction);
   addTransaction.addEventListener("click", add);
-  
+  */
   
   
 });
